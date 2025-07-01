bc6a3c581f923dfa608449aba797e3005336e1dc
root <pirouz.radin@yahoo.com>
2025-07-01 23:56:42 +0330

    remove somthings

--- Web Development & Frameworks/Django/info/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path("memory",  MemoryInfoView.as_view(),name="memory"),
-    path("cpu", CpuInfoView.as_view(),name="cpu"),
-    path("hard",HardInfoView.as_view(),name='hard'),
-]
